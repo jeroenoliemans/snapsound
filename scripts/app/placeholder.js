6f8ecfd84cc3ce0acf0cf4d2d5525af0dfb5b4b5
jeroenoliemans <jeroenoliemans@gmail.com>
2015-01-10 08:34:20 +0100

    added canvas image handler

--- scripts/app/placeholder.js
+++ scripts/app/placeholder.js
@@ -1,10 +1,29 @@
 define(function () {
     //Do setup work here
-    var loadImage = function(){
-      console.log('loadimage');
-    }
+    var canvas = document.querySelector('#canvas'),
+    ctx = canvas.getContext("2d"),
+    img = new Image(),
+    initPlaceholder = function(){
+      //set handler for upload button
+      document.querySelector('#btnUpload').onchange = function handleImage(e) {
+        var reader = new FileReader();
+          reader.onload = function (event){
+            img.src = event.target.result;
+            img.onload = function () {
+              addImageToCanvas();
+            }
+          }
+          reader.readAsDataURL(e.target.files[0]);
+        }
+    },
+    addImageToCanvas = function(){
+      ctx.drawImage(img,0,0,img.width,img.height,0,0,canvas.width,canvas.height);
+    },
+    hideUploadControls = function(){
+
+    };
 
     return {
-        loadImage: loadImage
+        initPlaceholder: initPlaceholder
     }
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 require(["app/placeholder", "app/controlpanel"], function(placeholder, controlpanel) {
     controlpanel.initControls();
+    placeholder.initPlaceholder();
 });
